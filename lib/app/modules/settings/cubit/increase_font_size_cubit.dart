2a91e50e64736270a7498892fcfe05079de80ac1
mr-santos <mr-santos@mr-santos.mr-santos>
2023-12-20 10:58:55 -0300

    Salvando preferencias do usuário

--- lib/app/modules/settings/cubit/increase_font_size_cubit.dart
+++ lib/app/modules/settings/cubit/increase_font_size_cubit.dart
@@ -1,19 +1,27 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:holy_bible/app/constants/constants.dart';
 
+import '../../../utils/app_preferences.dart';
+
 class IncreaseFontSizeCubit extends Cubit<double> {
   IncreaseFontSizeCubit() : super(Constants.fontSizeMin);
 
-  void increment() {
+  void increment() async {
     if(state < Constants.fontSizeMax ) {
       emit(state + 1);
+      await AppPreferences().setFontSize(state + 1);
     }
   } 
     
-  void decrement(){
+  void decrement() async {
     if(state > Constants.fontSizeMin){
       emit(state - 1);
+      await AppPreferences().setFontSize(state - 1);
     }
   } 
-  void updateSliderValue(double value) => emit(value);
+
+  void updateSliderValue(double value) async {
+    emit(value);
+    await AppPreferences().setFontSize(value);
+  } 
 }
--- lib/app/modules/settings/cubit/theme_mode_cubit.dart
+++ lib/app/modules/settings/cubit/theme_mode_cubit.dart
@@ -2,13 +2,15 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
 import '../../../constants/constants.dart';
+import '../../../utils/app_preferences.dart';
 
 enum ThemeModeEvent { toggle }
 
 class ThemeModeCubit extends Cubit<ThemeMode> {
   ThemeModeCubit() : super(Constants.themeApp);
 
-  void toggleTheme() {
+  void toggleTheme() async {
     emit(state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light);
+    await AppPreferences().setThemeMode(state);
   }
 }
